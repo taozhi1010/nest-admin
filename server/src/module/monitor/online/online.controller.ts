77fd5486384e84ac3580d68ab91522b735b31348
陶志 <taozhi@mucang.cn>
2024-06-14 11:03:30 +0800

    feat: 在线用户-添加接口权限

--- server/src/module/monitor/online/online.controller.ts
+++ server/src/module/monitor/online/online.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get, Param, Delete, Query } from '@nestjs/common';
 import { OnlineService } from './online.service';
 import { ApiTags, ApiOperation, ApiBody } from '@nestjs/swagger';
 import { OnlineListDto } from './dto/index';
+import { RequirePermission } from 'src/common/decorators/require-premission.decorator';
+
 @ApiTags('系统监控-在线用户')
 @Controller('monitor/online')
 export class OnlineController {
@@ -13,6 +15,7 @@ export class OnlineController {
   @ApiBody({
     type: OnlineListDto,
   })
+  @RequirePermission('monitor:online:query')
   @Get('/list')
   findAll(@Query() query) {
     return this.onlineService.findAll(query);
@@ -21,6 +24,7 @@ export class OnlineController {
   @ApiOperation({
     summary: '在线用户-强退',
   })
+  @RequirePermission('monitor:online:forceLogout')
   @Delete('/:token')
   delete(@Param('token') token: string) {
     return this.onlineService.delete(token);
