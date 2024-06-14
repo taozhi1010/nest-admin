17a9e31185693b251cecb0b0e3b2895aafdb437f
陶志 <taozhi@mucang.cn>
2024-06-14 10:51:48 +0800

    feat：菜单管理-添加接口权限

--- server/src/module/system/menu/menu.controller.ts
+++ server/src/module/system/menu/menu.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get, Post, Body, Query, Put, Param, Delete } from '@nestjs/
 import { ApiTags, ApiOperation, ApiBody, ApiConsumes, ApiQuery, ApiBearerAuth } from '@nestjs/swagger';
 import { MenuService } from './menu.service';
 import { CreateMenuDto, UpdateMenuDto, ListDeptDto } from './dto/index';
+import { RequirePermission } from 'src/common/decorators/require-premission.decorator';
+
 @ApiTags('菜单管理')
 @Controller('system/menu')
 export class MenuController {
@@ -14,6 +16,7 @@ export class MenuController {
     type: CreateMenuDto,
     required: true,
   })
+  @RequirePermission('system:menu:add')
   @Post()
   create(@Body() createMenuDto: CreateMenuDto) {
     return this.menuService.create(createMenuDto);
@@ -22,6 +25,7 @@ export class MenuController {
   @ApiOperation({
     summary: '菜单管理-列表',
   })
+  @RequirePermission('system:menu:query')
   @Get('/list')
   findAll(@Query() query: ListDeptDto) {
     return this.menuService.findAll(query);
@@ -30,6 +34,7 @@ export class MenuController {
   @ApiOperation({
     summary: '菜单管理-树表',
   })
+  @RequirePermission('system:menu:query')
   @Get('/treeselect')
   treeSelect() {
     return this.menuService.treeSelect();
@@ -38,17 +43,19 @@ export class MenuController {
   @ApiOperation({
     summary: '菜单管理-角色-树表',
   })
-  @Get('/roleMenuTreeselect/:id')
-  roleMenuTreeselect(@Param('id') id: string) {
-    return this.menuService.roleMenuTreeselect(+id);
+  @RequirePermission('system:menu:query')
+  @Get('/roleMenuTreeselect/:menuId')
+  roleMenuTreeselect(@Param('menuId') menuId: string) {
+    return this.menuService.roleMenuTreeselect(+menuId);
   }
 
   @ApiOperation({
     summary: '菜单管理-详情',
   })
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.menuService.findOne(+id);
+  @RequirePermission('system:menu:query')
+  @Get(':menuId')
+  findOne(@Param('menuId') menuId: string) {
+    return this.menuService.findOne(+menuId);
   }
 
   @ApiOperation({
@@ -58,6 +65,7 @@ export class MenuController {
     type: UpdateMenuDto,
     required: true,
   })
+  @RequirePermission('system:menu:edit')
   @Put()
   update(@Body() updateMenuDto: UpdateMenuDto) {
     return this.menuService.update(updateMenuDto);
@@ -66,8 +74,9 @@ export class MenuController {
   @ApiOperation({
     summary: '菜单管理-删除',
   })
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.menuService.remove(+id);
+  @RequirePermission('system:menu:remove')
+  @Delete(':menuId')
+  remove(@Param('menuId') menuId: string) {
+    return this.menuService.remove(+menuId);
   }
 }
