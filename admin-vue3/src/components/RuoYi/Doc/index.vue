57a016d21ed17d26d9e7a23c40eda07ee7c830c7
陶志 <taozhi@mucang.cn>
2024-07-04 14:30:20 +0800

    feat：vue3版本首页说明修改

--- admin-vue3/src/components/RuoYi/Doc/index.vue
+++ admin-vue3/src/components/RuoYi/Doc/index.vue
@@ -5,9 +5,9 @@
 </template>
 
 <script setup>
-const url = ref('http://doc.ruoyi.vip/ruoyi-vue');
+const url = ref('https://nest-admin.dooring.vip/prod-api/swagger-ui');
 
 function goto() {
-  window.open(url.value)
+  window.open(url.value);
 }
 </script>
