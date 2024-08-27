6ba99235ac8bbf9d5d3d9f4a2b0e44aca7c8a91b
陶志 <taozhi@mucang.cn>
2024-08-27 22:16:35 +0800

    feat：错误信息处理

--- server/src/common/filters/http-exceptions-filter.ts
+++ server/src/common/filters/http-exceptions-filter.ts
@@ -1,16 +1,22 @@
-import { Catch, HttpException, ExceptionFilter, ArgumentsHost } from '@nestjs/common';
+import { Catch, HttpException, ExceptionFilter, ArgumentsHost, HttpStatus } from '@nestjs/common';
 
 @Catch(HttpException)
 export class HttpExceptionsFilter implements ExceptionFilter {
   catch(exception: any, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
-    const status = exception.getStatus();
+    const status = HttpStatus.INTERNAL_SERVER_ERROR;
+    //exception.getStatus();
     const exceptionResponse = exception.getResponse();
+    let message = 'Service Error';
+
+    if (exceptionResponse?.message instanceof Array) {
+      message = exceptionResponse.message[0];
+    }
 
     response.status(200).json({
       code: status,
-      msg: exceptionResponse?.message ? exception.message : 'Service Error',
+      msg: message,
       data: null,
     });
   }
