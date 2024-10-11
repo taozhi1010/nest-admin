6b103b2df1d819a54fb46b854a546f4965afab9b
杨扬 <1076535690@qq.com>
2024-10-11 19:27:35 +0800

    fix: 修复读取配置错误的问题

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -13,5 +13,6 @@ const env = process.env.NODE_ENV;
 console.log(env);
 
 export default () => {
-  return yaml.load(readFileSync(join(__dirname, `./${configFileNameObj[env]}.yml`), 'utf8')) as Record<string, any>;
+  // return yaml.load(readFileSync(join(__dirname, `./${configFileNameObj[env]}.yml`), 'utf8')) as Record<string, any>;
+  return yaml.load(readFileSync(join(__dirname, `./dev.yml`), 'utf8')) as Record<string, any>;
 };
