5631a539664727f2cb9216662d7eb0d017eed415
crazystudent13 <1076535690@qq.com>
2024-10-25 19:33:07 +0800

    feat: 添加配置环境功能

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -10,9 +10,6 @@ const configFileNameObj = {
 
 const env = process.env.NODE_ENV;
 
-console.log(env);
-
 export default () => {
-  // return yaml.load(readFileSync(join(__dirname, `./${configFileNameObj[env]}.yml`), 'utf8')) as Record<string, any>;
-  return yaml.load(readFileSync(join(__dirname, `./dev.yml`), 'utf8')) as Record<string, any>;
+  return yaml.load(readFileSync(join(__dirname, `./env/${configFileNameObj[env]}.yml`), 'utf8')) as Record<string, any>;
 };
