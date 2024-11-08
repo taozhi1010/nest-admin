09ca5d948dabe59bbadbf247792b2f107e62f5b1
陶志 <taozhi@mucang.cn>
2024-11-08 16:42:59 +0800

    fix：代码生成格式优化

--- server/src/module/system/tool/template/nestjs/controller.ts
+++ server/src/module/system/tool/template/nestjs/controller.ts
@@ -1,56 +1,57 @@
+import * as Lodash from 'lodash';
 export const controllerTem = (options) => {
   const { BusinessName, businessName, functionName, moduleName } = options;
-  const serviceName = `${BusinessName}Service`;
+  const serviceName = `${Lodash.upperFirst(BusinessName)}Service`;
   const serviceInstance = `${businessName}Service`;
-
   return `
-    import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-    import { ${serviceName} } from './${businessName}.service';
-    import { Create${BusinessName}Dto,Update${BusinessName}Dto } from './dto/${businessName}.dto';
+import { Controller, Get, Post, Put, Body, Patch, Param, Delete } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
+import { RequirePermission } from 'src/common/decorators/require-premission.decorator';
+import { ${serviceName} } from './${businessName}.service';
+import { Create${Lodash.upperFirst(BusinessName)}Dto, Update${Lodash.upperFirst(BusinessName)}Dto, List${Lodash.upperFirst(BusinessName)}Dto } from './dto/${businessName}.dto';
 
-    @Controller('${businessName}')
-    export class ${BusinessName}Controller {
-        constructor(private readonly ${serviceInstance}: ${serviceName}) {}
-        @ApiOperation({
-            summary: '${functionName}-创建',
-        })
-        @RequirePermission('${moduleName}:${businessName}:add')
-        @Post()
-        create(@Body() create${BusinessName}Dto: Create${BusinessName}Dto) {
-            return this.${serviceInstance}.create(create${BusinessName}Dto);
-        }
-        @ApiOperation({
-            summary: '${functionName}-列表',
-        })
-        @RequirePermission('${moduleName}:${businessName}:list')
-        @Get()
-        findAll() {
-            return this.${serviceInstance}.findAll();
-        }
-        @ApiOperation({
-            summary: '${functionName}-详情',
-        })
-        @RequirePermission('${moduleName}:${businessName}:query')
-        @Get(':id')
-        findOne(@Param('id') id: string) {
-            return this.${serviceInstance}.findOne(+id);
-        }
-        @ApiOperation({
-            summary: '${functionName}-修改',
-        })
-        @RequirePermission('${moduleName}:${businessName}:edit')
-        @Put()
-        update(@Body() update${BusinessName}Dto: Update${BusinessName}Dto) {
-            return this.${serviceInstance}.update(update${BusinessName}Dto);
-        }
-        @ApiOperation({
-            summary: '${functionName}-删除',
-        })
-        @RequirePermission('${moduleName}:${businessName}:remove')
-        @Delete(':id')
-        remove(@Param('id') id: string) {
-            return this.${serviceInstance}.remove(+id);
-        }
+@Controller('${businessName}')
+export class ${Lodash.upperFirst(BusinessName)}Controller {
+constructor(private readonly ${serviceInstance}: ${serviceName}) {}
+    @ApiOperation({
+        summary: '${functionName}-创建',
+    })
+    @RequirePermission('${moduleName}:${businessName}:add')
+    @Post()
+    create(@Body() create${Lodash.upperFirst(BusinessName)}Dto: Create${Lodash.upperFirst(BusinessName)}Dto) {
+        return this.${serviceInstance}.create(create${Lodash.upperFirst(BusinessName)}Dto);
+    }\n
+    @ApiOperation({
+        summary: '${functionName}-列表',
+    })
+    @RequirePermission('${moduleName}:${businessName}:list')
+    @Get()
+    findAll(@Body() list${Lodash.upperFirst(BusinessName)}Dto: List${Lodash.upperFirst(BusinessName)}Dto) {
+        return this.${serviceInstance}.findAll(list${Lodash.upperFirst(BusinessName)}Dto);
+    }\n
+    @ApiOperation({
+        summary: '${functionName}-详情',
+    })
+    @RequirePermission('${moduleName}:${businessName}:query')
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.${serviceInstance}.findOne(+id);
+    }\n
+    @ApiOperation({
+        summary: '${functionName}-修改',
+    })
+    @RequirePermission('${moduleName}:${businessName}:edit')
+    @Put()
+    update(@Body() update${Lodash.upperFirst(BusinessName)}Dto: Update${Lodash.upperFirst(BusinessName)}Dto) {
+        return this.${serviceInstance}.update(update${Lodash.upperFirst(BusinessName)}Dto);
+    }\n
+    @ApiOperation({
+        summary: '${functionName}-删除',
+    })
+    @RequirePermission('${moduleName}:${businessName}:remove')
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.${serviceInstance}.remove(+id);
     }
-    `;
+}`;
 };
--- server/src/module/system/tool/template/nestjs/entity.ts
+++ server/src/module/system/tool/template/nestjs/entity.ts
@@ -1,36 +1,34 @@
 import { GenConstants } from 'src/common/constant/genConstants';
+import * as Lodash from 'lodash';
+
 export const entityTem = (options) => {
   const { BusinessName, tableName, tableComment } = options;
 
   const contentTem = content(options);
   return `
-    import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-    import { ApiProperty } from '@nestjs/swagger';
-    import { BaseEntity } from 'src/common/entities/base';
-    @Entity('${tableName}', {
-        comment: '${tableComment}',
-      })
-    export class ${BusinessName}Entity extends BaseEntity {
-        ${contentTem}
-    }
-    `;
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('${tableName}', {
+    comment: '${tableComment}',
+})
+export class ${Lodash.upperFirst(BusinessName)}Entity {
+${contentTem}
+}
+`;
 };
 
 const content = (options) => {
   const { columns } = options;
   let html = ``;
+  columns.sort((a, b) => b.isPk - a.isPk); //保证主键始终在第一个
   columns.forEach((column) => {
     const { javaType, javaField, isPk, columnType, columnComment } = column;
     const filed = convertToSnakeCase(javaField);
     const type = lowercaseFirstLetter(javaType);
     if (isPk == '1') {
-      html += `@PrimaryGeneratedColumn({ type: '${columnType}', name: '${filed}', comment: '${columnComment}' })
-            public ${javaField}: ${type};
-            `;
+      html += `\t@PrimaryGeneratedColumn({ type: '${columnType}', name: '${filed}', comment: '${columnComment}' })\n\tpublic ${javaField}: ${type};\n`;
     } else if (!GenConstants.BASE_ENTITY.includes(javaField)) {
-      html += `@Column({ type: '${columnType}', name: '${filed}', comment: '${columnComment}' })
-            public ${javaField}: ${type};
-            `;
+      html += `\n\t@Column({ type: '${columnType}', name: '${filed}', comment: '${columnComment}' })\n\tpublic ${javaField}: ${type};\n`;
     }
   });
 
@@ -44,6 +42,8 @@ function convertToSnakeCase(str) {
 function lowercaseFirstLetter(str) {
   if (str.length === 0) {
     return str;
+  } else if (str === 'Date') {
+    return 'Date';
   }
   return str.charAt(0).toLowerCase() + str.slice(1);
 }
