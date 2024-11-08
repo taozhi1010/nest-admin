80767bd75d0bf6e4619683914bd24728d5237ba8
陶志 <taozhi@mucang.cn>
2024-11-08 22:37:16 +0800

    feat：代码导出功能优化

--- server/src/module/system/tool/template/nestjs/dto.ts
+++ server/src/module/system/tool/template/nestjs/dto.ts
@@ -1,60 +1,49 @@
 import * as Lodash from 'lodash';
 export const dtoTem = (options) => {
   const { BusinessName, primaryKey } = options;
-  const req = dtoIsReq(options);
-  const list = listDtoTem(options);
+  const insert = dtoGenerate(options, 'isInsert');
+  const edit = dtoGenerate(options, 'isEdit');
+  const list = dtoGenerate(options, 'isQuery');
   return `
 import { IsString, IsNumber, IsBoolean, IsDate, IsOptional, IsEnum } from 'class-validator';
 import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { PagingDto } from 'src/common/dto/index';
+
+export enum CharEnum {
+  ENABLE = '0',
+  DISABLE = '1',
+}
 
 export class Create${Lodash.upperFirst(BusinessName)}Dto {
-${req}
+${insert}
 }
 
-export class Update${Lodash.upperFirst(BusinessName)}Dto extends PartialType(Create${Lodash.upperFirst(BusinessName)}Dto) {
-    @ApiProperty({ required: true })
-    @IsNumber()
-    ${primaryKey}: number;
+export class Update${Lodash.upperFirst(BusinessName)}Dto{
+ ${edit}
 }
 
-export class List${Lodash.upperFirst(BusinessName)}Dto {
-    @ApiProperty({ required: true })
-    @IsNumber()
-    pageNum: number;
-
-    @ApiProperty({ required: true })
-    @IsNumber()
-    pageSize: number;
-
+export class List${Lodash.upperFirst(BusinessName)}Dto extends PagingDto{
 ${list}
 }
 `;
 };
 
-const dtoIsReq = (options) => {
+const dtoGenerate = (options, type) => {
   const { columns } = options;
   return columns
-    .filter((column) => column.isPk !== '1')
+    .filter((column) => column[type] === '1')
     .map((column) => {
-      const { javaType, javaField, isRequired } = column;
+      const { javaType, javaField, isRequired, columnComment, columnType } = column;
       const type = lowercaseFirstLetter(javaType);
-      const decorators = [`@ApiProperty({ required: ${isRequired == 1} })`, isRequired != 1 && `\t@IsOptional()`, '\t' + getValidatorDecorator(javaType)].filter(Boolean).join('\n');
+      const decorators = [
+        `@ApiProperty({${columnType === 'char' ? 'enum: CharEnum, ' : ''}required: ${isRequired == 1} , description: '${columnComment}'})`,
+        isRequired != 1 && `\t@IsOptional()`,
+        '\t' + getValidatorDecorator(javaType),
+      ]
+        .filter(Boolean)
+        .join('\n');
 
       return `\t${decorators}\n\t${javaField}${isRequired == 1 ? '' : '?'}: ${type == 'Date' ? javaType : type};\n`;
-    })
-    .join('\n');
-};
-
-const listDtoTem = (options) => {
-  const { columns } = options;
-  return columns
-    .filter((column) => column.isQuery == '1')
-    .map((column) => {
-      const { javaType, javaField, isRequired } = column;
-      const type = lowercaseFirstLetter(javaType);
-      const decorators = [`@ApiProperty({ required: ${isRequired == 1} })`, isRequired != 1 && '\t@IsOptional()', '\t' + getValidatorDecorator(javaType)].filter(Boolean).join('\n');
-
-      return `\t${decorators}\n\t${javaField}${isRequired == 1 ? '' : '?'}: ${type};\n`;
     })
     .join('\n');
 };
@@ -76,7 +65,7 @@ function getValidatorDecorator(javaType) {
 
 function lowercaseFirstLetter(str) {
   if (str === 'Date') {
-    return 'Date';
+    return str;
   }
   return str.charAt(0).toLowerCase() + str.slice(1);
 }
