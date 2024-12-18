bdbf5099afb9a8cca628e20ee301816441e3a247
crazystudent13 <1076535690@qq.com>
2024-12-18 13:23:39 +0800

    build(auto-import): 添加 dayjs 自动导入配置
    - 在 auto-import 插件中添加 dayjs 库的自动导入

--- admin-vue3/vite/plugins/auto-import.js
+++ admin-vue3/vite/plugins/auto-import.js
@@ -1,12 +1,15 @@
 import autoImport from 'unplugin-auto-import/vite'
 
 export default function createAutoImport() {
-    return autoImport({
-        imports: [
-            'vue',
-            'vue-router',
-            'pinia'
-        ],
-        dts: false
-    })
+  return autoImport({
+    imports: [
+      'vue',
+      'vue-router',
+      'pinia',
+      {
+        dayjs: [['default', 'dayjs']]
+      }
+    ],
+    dts: false
+  })
 }
