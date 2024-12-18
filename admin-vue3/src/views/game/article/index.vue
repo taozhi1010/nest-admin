b079c0a1e1099eea3374c546d0c9e7991d1371d8
crazystudent13 <1076535690@qq.com>
2024-12-18 11:19:39 +0800

    feat(game): 初步添加文章管理功能
    - 新增文章管理相关的 API 接口
    - 实现文章列表查询、文章详情获取、文章新增、修改和删除功能
    - 添加文章管理页面，包括搜索、分页、操作按钮等功能

---
+++ admin-vue3/src/views/game/article/index.vue
@@ -0,0 +1,236 @@
+<template>
+  <div class="app-container">
+    <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch">
+      <el-form-item label="文章标题" prop="title">
+        <el-input v-model="queryParams.title" placeholder="请输入文章标题" clearable style="width: 200px" @keyup.enter="handleQuery" />
+      </el-form-item>
+      <!-- todo: 状态字典翻译,数据字典有问题，目前的数据字典改完数据之后，前端的数据字典没有及时修改数据键值 -->
+      <!-- <el-form-item label="状态" prop="status">
+        <el-select v-model="queryParams.status" placeholder="文章状态" clearable style="width: 200px">
+          <el-option v-for="dict in sys_article_status" :key="dict.value" :label="dict.label" :value="dict.value" />
+        </el-select>
+      </el-form-item> -->
+      <el-form-item>
+        <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
+        <el-button icon="Refresh" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button type="primary" plain icon="Plus" @click="handleAdd" v-hasPermi="['game:Article:add']">新增</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button type="success" plain icon="Edit" :disabled="single" @click="handleUpdate" v-hasPermi="['game:Article:edit']">修改</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button type="danger" plain icon="Delete" :disabled="multiple" @click="handleDelete" v-hasPermi="['game:Article:remove']">删除</el-button>
+      </el-col>
+      <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"></right-toolbar>
+    </el-row>
+
+    <el-table v-loading="loading" :data="ArticleList" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" align="center" />
+      <el-table-column label="文章标题" align="center" prop="title" show-overflow-tooltip />
+      <el-table-column label="文章简介" align="center" prop="remark" show-overflow-tooltip />
+      <el-table-column label="文章作者" align="center" prop="author" />
+      <el-table-column v-if="false" label="状态" align="center" prop="status">
+        <template #default="scope">
+          <!-- <dict-tag :options="sys_article_status" :value="scope.row.status" /> -->
+        </template>
+      </el-table-column>
+      <el-table-column label="发布时间" align="center" prop="publishTime" width="180">
+        <template #default="scope">
+          <span>{{ dayjs(scope.row.publishTime).format('YYYY-MM-DD HH:mm:ss') }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="创建时间" align="center" prop="createTime" width="180">
+        <template #default="scope">
+          <span>{{ dayjs(scope.row.createTime).format('YYYY-MM-DD HH:mm:ss') }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="修改时间" align="center" prop="updateTime" width="180">
+        <template #default="scope">
+          <span>{{ dayjs(scope.row.updateTime).format('YYYY-MM-DD HH:mm:ss') }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" width="180" align="center" class-name="small-padding fixed-width">
+        <template #default="scope">
+          <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['game:Article:edit']">修改</el-button>
+          <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['game:Article:remove']">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <pagination v-show="total > 0" :total="total" v-model:page="queryParams.pageNum" v-model:limit="queryParams.pageSize" @pagination="getList" />
+
+    <!-- 添加或修改岗位对话框 -->
+    <el-dialog :title="title" v-model="open" width="500px" append-to-body>
+      <el-form ref="ArticleRef" :model="form" :rules="rules" label-width="80px">
+        <el-form-item label="文章标题" prop="title">
+          <el-input v-model="form.title" placeholder="请输入岗位名称" />
+        </el-form-item>
+        <el-form-item label="文章简介" prop="remark">
+          <el-input v-model="form.remark" placeholder="请输入编码名称" />
+        </el-form-item>
+        <!-- todo: 文件上传公用组件的封装 -->
+        <!-- <el-form-item label="文章封面" prop="remark">
+          <el-input v-model="form.remark" placeholder="请输入编码名称" />
+        </el-form-item> -->
+        <el-form-item label="文章状态" prop="status">
+          <el-radio-group v-model="form.status">
+            <el-radio v-for="dict in sys_article_status" :key="dict.value" :label="dict.value">{{ dict.label }}</el-radio>
+          </el-radio-group>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button type="primary" @click="submitForm">确 定</el-button>
+          <el-button @click="cancel">取 消</el-button>
+        </div>
+      </template>
+    </el-dialog>
+  </div>
+</template>
+
+<script setup name="Article">
+import dayjs from 'dayjs'
+import { listArticle, addArticle, delArticle, getArticle, updateArticle } from '@/api/game/Article'
+
+const { proxy } = getCurrentInstance()
+const { sys_article_status } = proxy.useDict('sys_article_status')
+console.log(sys_article_status, '数据字典')
+
+const ArticleList = ref([])
+const open = ref(false)
+const loading = ref(true)
+const showSearch = ref(true)
+const ids = ref([])
+const single = ref(true)
+const multiple = ref(true)
+const total = ref(0)
+const title = ref('')
+
+const data = reactive({
+  form: {},
+  queryParams: {
+    pageNum: 1,
+    pageSize: 10,
+    ArticleCode: undefined,
+    ArticleName: undefined,
+    status: undefined
+  },
+  rules: {
+    ArticleName: [{ required: true, message: '岗位名称不能为空', trigger: 'blur' }],
+    ArticleCode: [{ required: true, message: '岗位编码不能为空', trigger: 'blur' }],
+    ArticleSort: [{ required: true, message: '岗位顺序不能为空', trigger: 'blur' }]
+  }
+})
+
+const { queryParams, form, rules } = toRefs(data)
+
+/** 查询岗位列表 */
+function getList() {
+  loading.value = true
+  listArticle(queryParams.value).then((response) => {
+    ArticleList.value = response.data.list
+    total.value = response.data.total
+    loading.value = false
+  })
+}
+/** 取消按钮 */
+function cancel() {
+  open.value = false
+  reset()
+}
+/** 表单重置 */
+function reset() {
+  form.value = {
+    articleId: undefined,
+    ArticleCode: undefined,
+    ArticleName: undefined,
+    ArticleSort: 0,
+    status: '0',
+    remark: undefined
+  }
+  proxy.resetForm('ArticleRef')
+}
+/** 搜索按钮操作 */
+function handleQuery() {
+  queryParams.value.pageNum = 1
+  getList()
+}
+/** 重置按钮操作 */
+function resetQuery() {
+  proxy.resetForm('queryRef')
+  handleQuery()
+}
+/** 多选框选中数据 */
+function handleSelectionChange(selection) {
+  ids.value = selection.map((item) => item.articleId)
+  single.value = selection.length != 1
+  multiple.value = !selection.length
+}
+/** 新增按钮操作 */
+function handleAdd() {
+  reset()
+  open.value = true
+  title.value = '添加岗位'
+}
+/** 修改按钮操作 */
+function handleUpdate(row) {
+  reset()
+  const articleId = row.articleId || ids.value
+  getArticle(articleId).then((response) => {
+    form.value = response.data
+    open.value = true
+    title.value = '修改岗位'
+  })
+}
+/** 提交按钮 */
+function submitForm() {
+  proxy.$refs['ArticleRef'].validate((valid) => {
+    if (valid) {
+      if (form.value.articleId != undefined) {
+        updateArticle(form.value).then((response) => {
+          proxy.$modal.msgSuccess('修改成功')
+          open.value = false
+          getList()
+        })
+      } else {
+        addArticle(form.value).then((response) => {
+          proxy.$modal.msgSuccess('新增成功')
+          open.value = false
+          getList()
+        })
+      }
+    }
+  })
+}
+/** 删除按钮操作 */
+function handleDelete(row) {
+  const articleIds = row.articleId || ids.value
+  proxy.$modal
+    .confirm('是否确认删除岗位编号为"' + articleIds + '"的数据项？')
+    .then(function () {
+      return delArticle(articleIds)
+    })
+    .then(() => {
+      getList()
+      proxy.$modal.msgSuccess('删除成功')
+    })
+    .catch(() => {})
+}
+/** 导出按钮操作 */
+function handleExport() {
+  proxy.download(
+    'game/Article/export',
+    {
+      ...queryParams.value
+    },
+    `Article_${new Date().getTime()}.xlsx`
+  )
+}
+
+getList()
+</script>
