c4ef59d437e58824e96319ac6014b4e2eecaca60
杨扬 <1076535690@qq.com>
2025-05-09 21:10:28 +0800

    style(admin-vue3): 优化代码格式和结构
    - 使用 ESLint 规则格式化代码
    - 调整缩进、空格和换行
    - 统一字符串引号使用单引号
    - 移除冗余的空行和空格
    - 优化部分变量和函数的命名

--- admin-vue3/src/layout/components/AppMain.vue
+++ admin-vue3/src/layout/components/AppMain.vue
@@ -2,9 +2,9 @@
   <section class="app-main">
     <router-view v-slot="{ Component, route }">
       <!-- <transition name="fade-transform" mode="out-in"> -->
-        <keep-alive :include="tagsViewStore.cachedViews">
-          <component v-if="!route.meta.link" :is="Component" :key="route.path"/>
-        </keep-alive>
+      <keep-alive :include="tagsViewStore.cachedViews">
+        <component v-if="!route.meta.link" :is="Component" :key="route.path" />
+      </keep-alive>
       <!-- </transition> -->
     </router-view>
     <iframe-toggle />
@@ -12,7 +12,7 @@
 </template>
 
 <script setup>
-import iframeToggle from "./IframeToggle/index"
+import iframeToggle from './IframeToggle/index'
 import useTagsViewStore from '@/store/modules/tagsView'
 
 const tagsViewStore = useTagsViewStore()
@@ -65,4 +65,3 @@ const tagsViewStore = useTagsViewStore()
   border-radius: 3px;
 }
 </style>
-
