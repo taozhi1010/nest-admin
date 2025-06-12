2b9d63439c7b584a3753ed1052e457775eefd6cb
crazystudent13 <1076535690@qq.com>
2025-06-12 19:14:19 +0800

    refactor(notice): 重构公告模块，初步抽离table部分的hooks
    - 新增 useTable 钩子实现表格数据请求和分页功能
    - 优化公告列表和表单组件代码结构
    - 移除冗余代码，提高代码可读性和维护性

---
+++ admin-vue3/src/hooks/useTable.ts
@@ -0,0 +1,111 @@
+import { reactive } from 'vue'
+
+// 后续参考一下这个
+// https://www.buerblog.cn/docs/study/web/use-table
+
+/**
+ * 定义pageInfo
+ */
+interface page {
+  pageNum: number
+  pageSize: number
+  total: number
+}
+
+/**
+ * 定义数据
+ */
+interface TableState {
+  page: page
+  loading: boolean
+  list: any[]
+}
+
+/**
+ * @description table操作方法封装
+ * @param {Function} api 表格列表数据接口
+ * @param {Object} searchParam 表格查询参数
+ * @param {Object} formRef 表单ref,用于处理清空form的校验结果等操作
+ */
+
+const useTable = (api, searchParam, formRef: any) => {
+  const state = reactive<TableState>({
+    loading: false,
+    list: [],
+    page: {
+      pageNum: 1,
+      pageSize: 20,
+      total: 1
+    }
+  })
+
+  // 获取表格列表数据
+  const request = async () => {
+    const params = {
+      pageNum: state.page.pageNum,
+      pageSize: state.page.pageSize,
+      ...searchParam
+    }
+
+    state.loading = true
+    try {
+      const { code, data } = await api(params)
+      if (code === 200) {
+        // todo：因为reative的问题，直接清空和赋值无效，只能用push方法，希望能有更优雅的方法
+        state.list.length = 0
+        state.list.push(...data.list)
+        state.page.total = data.total
+      }
+    } catch (e) {
+      console.log('list error：', e)
+    } finally {
+      state.loading = false
+    }
+  }
+
+  // 分页页码切换
+  const onPageChange = (page: number) => {
+    state.page.pageNum = page
+    request()
+  }
+
+  // 分页大小切换
+  const onSizeChange = (size: number) => {
+    state.page.pageSize = size
+    request()
+  }
+
+  // 搜索
+  const onSearch = () => {
+    state.page.pageNum = 1
+    request()
+  }
+
+  // 重置搜索
+  const onReset = () => {
+    state.page.pageNum = 1
+    formRef.value.resetFields()
+    request()
+  }
+
+  // 刷新
+  const onRefresh = () => {
+    request()
+  }
+
+  // 初始化请求数据
+  request()
+
+  // 返回相关变量与方法
+  return {
+    state,
+    request,
+    onSizeChange,
+    onPageChange,
+    onSearch,
+    onReset,
+    onRefresh
+  }
+}
+
+export default useTable
