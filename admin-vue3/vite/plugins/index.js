ecb3fba63ab496cb1b8c42e419fe266b68bc4006
Leo <taozhi1010@163.com>
2025-07-26 09:11:09 +0800

    feat: admin增加CodeInspectorPlugin插件

--- admin-vue3/vite/plugins/index.js
+++ admin-vue3/vite/plugins/index.js
@@ -4,12 +4,14 @@ import createAutoImport from './auto-import'
 import createSvgIcon from './svg-icon'
 import createCompression from './compression'
 import createSetupExtend from './setup-extend'
+import { CodeInspectorPlugin } from 'code-inspector-plugin'
 
 export default function createVitePlugins(viteEnv, isBuild = false) {
-    const vitePlugins = [vue()]
-    vitePlugins.push(createAutoImport())
-	vitePlugins.push(createSetupExtend())
-    vitePlugins.push(createSvgIcon(isBuild))
-	isBuild && vitePlugins.push(...createCompression(viteEnv))
-    return vitePlugins
+  const vitePlugins = [vue()]
+  vitePlugins.push(createAutoImport())
+  vitePlugins.push(createSetupExtend())
+  vitePlugins.push(createSvgIcon(isBuild))
+  vitePlugins.push(CodeInspectorPlugin({ bundler: 'vite', editor: 'trae' }))
+  isBuild && vitePlugins.push(...createCompression(viteEnv))
+  return vitePlugins
 }
