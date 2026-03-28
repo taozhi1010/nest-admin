d21780ed199df716f94022c222e537f3955e055e
crazystudent13 <1076535690@qq.com>
2026-03-29 07:58:06 +0800

    fix: 修复 useCommon 导出问题，支持单独导入工具函数
    - 添加 parseTime, resetForm, addDateRange 等函数的单独导出
    - 保持 useCommon Composable 的向后兼容
    - 支持两种使用方式：
      1. import { addDateRange } from '@/composables/useCommon'
      2. const { addDateRange } = useCommon()

--- admin-vue3/src/composables/useCommon.ts
+++ admin-vue3/src/composables/useCommon.ts
@@ -1,4 +1,12 @@
-﻿import { parseTime, resetForm, addDateRange, handleTree, selectDictLabel, selectDictLabels } from '@/utils/ruoyi'
+﻿import { parseTime as _parseTime, resetForm as _resetForm, addDateRange as _addDateRange, handleTree as _handleTree, selectDictLabel as _selectDictLabel, selectDictLabels as _selectDictLabels } from '@/utils/ruoyi'
+
+// 单独导出各个工具函数
+export const parseTime = _parseTime
+export const resetForm = _resetForm
+export const addDateRange = _addDateRange
+export const handleTree = _handleTree
+export const selectDictLabel = _selectDictLabel
+export const selectDictLabels = _selectDictLabels
 
 /**
  * 通用工具函数 Composable
