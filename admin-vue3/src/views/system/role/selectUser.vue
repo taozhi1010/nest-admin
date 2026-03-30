9236cec279f970812bda9663001ffeb68206c27f
crazystudent13 <1076535690@qq.com>
2026-03-30 23:20:42 +0800

    refactor(role): 重构角色管理模块数据绑定和方法调用
    - 将组件数据和方法统一绑定到 role 对象下
    - 更新模板中所有数据绑定和事件调用前缀
    - 优化方法调用方式，统一使用 role 对象调用
    - 调整代码结构，增加注释分组便于维护
    - 修复部分方法中的异步处理逻辑

--- admin-vue3/src/views/system/role/selectUser.vue
+++ admin-vue3/src/views/system/role/selectUser.vue
@@ -1,20 +1,23 @@
 <template>
   <!-- 授权用户 -->
-  <el-dialog title="选择用户" v-model="visible" width="800px" top="5vh" append-to-body>
-    <el-form :model="queryParams" ref="queryRef" :inline="true">
+  <el-dialog title="选择用户" v-model="selectUser.visible" width="800px" top="5vh" append-to-body>
+    <el-form :model="selectUser.queryParams" ref="selectUser.queryRef" :inline="true">
       <el-form-item label="用户账号" prop="userName">
-        <el-input v-model="queryParams.userName" placeholder="请输入用户账号" clearable style="width: 200px" @keyup.enter="handleQuery" />
+        <el-input v-model.trim="selectUser.queryParams.userName" placeholder="请输入用户账号" clearable style="width: 200px"
+          @keyup.enter="selectUser.handleQuery" />
       </el-form-item>
       <el-form-item label="手机号码" prop="phonenumber">
-        <el-input v-model="queryParams.phonenumber" placeholder="请输入手机号码" clearable style="width: 200px" @keyup.enter="handleQuery" />
+        <el-input v-model.trim="selectUser.queryParams.phonenumber" placeholder="请输入手机号码" clearable style="width: 200px"
+          @keyup.enter="selectUser.handleQuery" />
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
-        <el-button icon="Refresh" @click="resetQuery">重置</el-button>
+        <el-button type="primary" icon="Search" @click="selectUser.handleQuery">搜索</el-button>
+        <el-button icon="Refresh" @click="selectUser.resetQuery">重置</el-button>
       </el-form-item>
     </el-form>
     <el-row>
-      <el-table @row-click="clickRow" ref="refTable" :data="userList" @selection-change="handleSelectionChange" height="260px">
+      <el-table @row-click="selectUser.clickRow" ref="refTable" :data="selectUser.userList"
+        @selection-change="selectUser.handleSelectionChange" height="260px">
         <el-table-column type="selection" width="55"></el-table-column>
         <el-table-column label="用户账号" prop="userName" :show-overflow-tooltip="true" />
         <el-table-column label="用户昵称" prop="nickName" :show-overflow-tooltip="true" />
@@ -31,95 +34,123 @@
           </template>
         </el-table-column>
       </el-table>
-      <pagination v-show="total > 0" :total="total" v-model:page="queryParams.pageNum" v-model:limit="queryParams.pageSize" @pagination="getList" />
+      <pagination v-show="selectUser.total > 0" :total="selectUser.total" v-model:page="selectUser.queryParams.pageNum"
+        v-model:limit="selectUser.queryParams.pageSize" @pagination="selectUser.getList" />
     </el-row>
     <template #footer>
       <div class="dialog-footer">
-        <el-button type="primary" @click="handleSelectUser">确 定</el-button>
-        <el-button @click="visible = false">取 消</el-button>
+        <el-button type="primary" @click="selectUser.handleSelectUser">确 定</el-button>
+        <el-button @click="selectUser.visible = false">取 消</el-button>
       </div>
     </template>
   </el-dialog>
 </template>
 
 <script setup name="SelectUser">
+// ==================== 导入区域 ====================
 import { authUserSelectAll, unallocatedUserList } from '@/api/system/role'
+import { useDict } from '@/composables/useDict'
+import { resetForm, parseTime } from '@/composables/useCommon'
 
+// ==================== Props 和 Emits ====================
 const props = defineProps({
   roleId: {
     type: [Number, String]
   }
 })
 
-const { proxy } = getCurrentInstance()
-import { useDict } from '@/composables/useDict'
-import { resetForm, parseTime } from '@/composables/useCommon'
+const emit = defineEmits(['ok'])
 
+// ==================== 实例和字典 ====================
+const { proxy } = getCurrentInstance()
 const { sys_normal_disable } = useDict('sys_normal_disable')
 
-const userList = ref([])
-const visible = ref(false)
-const total = ref(0)
-const userIds = ref([])
+// ==================== 表单引用 ====================
 const refTable = ref(null)
 
-const queryParams = reactive({
-  pageNum: 1,
-  pageSize: 10,
-  roleId: undefined,
-  userName: undefined,
-  phonenumber: undefined
+// ==================== 选择用户管理（集中式管理） ====================
+const selectUser = reactive({
+  // 响应式数据
+  queryRef: null,
+  userList: [],
+  visible: false,
+  total: 0,
+  userIds: [],
+
+  // 查询参数
+  queryParams: {
+    pageNum: 1,
+    pageSize: 10,
+    roleId: undefined,
+    userName: undefined,
+    phonenumber: undefined
+  },
+
+  // 方法集合
+  // 显示弹框
+  show: () => {
+    selectUser.queryParams.roleId = props.roleId
+    selectUser.getList()
+    selectUser.visible = true
+  },
+
+  // 选择行
+  clickRow: (row) => {
+    proxy.$refs['refTable'].toggleRowSelection(row)
+  },
+
+  // 多选框选中数据
+  handleSelectionChange: (selection) => {
+    selectUser.userIds = selection.map((item) => item.userId)
+  },
+
+  // 查询表数据
+  getList: async () => {
+    try {
+      const response = await unallocatedUserList(selectUser.queryParams)
+      selectUser.userList = response.data.list
+      selectUser.total = response.data.total
+    } catch (e) {
+      console.error('查询用户列表失败:', e)
+    }
+  },
+
+  // 搜索按钮操作
+  handleQuery: () => {
+    selectUser.queryParams.pageNum = 1
+    selectUser.getList()
+  },
+
+  // 重置按钮操作
+  resetQuery: () => {
+    resetForm(selectUser.queryRef)
+    selectUser.handleQuery()
+  },
+
+  // 选择授权用户操作
+  handleSelectUser: async () => {
+    const roleId = selectUser.queryParams.roleId
+    const uIds = selectUser.userIds.join(',')
+
+    if (uIds == '') {
+      proxy.$modal.msgError('请选择要分配的用户')
+      return
+    }
+
+    try {
+      const response = await authUserSelectAll({ roleId: +roleId, userIds: uIds })
+      proxy.$modal.msgSuccess(response.msg)
+      if (response.code === 200) {
+        selectUser.visible = false
+        emit('ok')
+      }
+    } catch (e) {
+      console.error('分配用户失败:', e)
+    }
+  }
 })
 
-// 显示弹框
-function show() {
-  queryParams.roleId = props.roleId
-  getList()
-  visible.value = true
-}
-/**选择行 */
-function clickRow(row) {
-  proxy.$refs['refTable'].toggleRowSelection(row)
-}
-// 多选框选中数据
-function handleSelectionChange(selection) {
-  userIds.value = selection.map((item) => item.userId)
-}
-// 查询表数据
-function getList() {
-  unallocatedUserList(queryParams).then((res) => {
-    userList.value = res.data.list
-    total.value = res.data.total
-  })
-}
-/** 搜索按钮操作 */
-function handleQuery() {
-  queryParams.pageNum = 1
-  getList()
-}
-/** 重置按钮操作 */
-function resetQuery() {
-  resetForm(queryRef)
-  handleQuery()
-}
-const emit = defineEmits(['ok'])
-/** 选择授权用户操作 */
-function handleSelectUser() {
-  const roleId = queryParams.roleId
-  const uIds = userIds.value.join(',')
-  if (uIds == '') {
-    proxy.$modal.msgError('请选择要分配的用户')
-    return
-  }
-  authUserSelectAll({ roleId: +roleId, userIds: uIds }).then((res) => {
-    proxy.$modal.msgSuccess(res.msg)
-    if (res.code === 200) {
-      visible.value = false
-      emit('ok')
-    }
-  })
-}
-
+// ==================== 暴露方法 ====================
 defineExpose({
   show
 })
