bd392697b68252404e9375e71aae2c957d0aafbd
crazystudent13 <1076535690@qq.com>
2026-04-04 23:31:57 +0800

    feat: 为在线用户页面添加 main-card 容器，统一页面样式主题

--- admin-vue3/src/views/monitor/online/index.vue
+++ admin-vue3/src/views/monitor/online/index.vue
@@ -1,43 +1,45 @@
 <template>
   <div class="app-container">
-    <el-form ref="queryRef" :inline="true" :model="queryParams">
-      <el-form-item label="登录地址" prop="ipaddr">
-        <el-input v-model="queryParams.ipaddr" clearable placeholder="请输入登录地址" style="width: 200px" @keyup.enter="handleQuery" />
-      </el-form-item>
-      <el-form-item label="用户账号" prop="userName">
-        <el-input v-model="queryParams.userName" clearable placeholder="请输入用户账号" style="width: 200px" @keyup.enter="handleQuery" />
-      </el-form-item>
-      <el-form-item>
-        <el-button icon="Search" type="primary" @click="handleQuery">搜索</el-button>
-        <el-button icon="Refresh" @click="resetQuery">重置</el-button>
-      </el-form-item>
-    </el-form>
-    <el-table v-loading="loading" :data="onlineList" style="width: 100%">
-      <el-table-column align="center" label="序号" type="index" width="50">
-        <template #default="scope">
-          <span>{{ (pageNum - 1) * pageSize + scope.$index + 1 }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column align="center" label="会话编号" prop="tokenId" :show-overflow-tooltip="true" />
-      <el-table-column align="center" label="登录账号" prop="userName" :show-overflow-tooltip="true" />
-      <el-table-column align="center" label="所属部门" prop="deptName" :show-overflow-tooltip="true" />
-      <el-table-column align="center" label="主机" prop="ipaddr" :show-overflow-tooltip="true" />
-      <el-table-column align="center" label="登录地点" prop="loginLocation" :show-overflow-tooltip="true" />
-      <el-table-column align="center" label="操作系统" prop="os" :show-overflow-tooltip="true" />
-      <el-table-column align="center" label="浏览器" prop="browser" :show-overflow-tooltip="true" />
-      <el-table-column align="center" label="登录时间" prop="loginTime" width="180">
-        <template #default="scope">
-          <span>{{ parseTime(scope.row.loginTime) }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column align="center" class-name="small-padding fixed-width" label="操作">
-        <template #default="scope">
-          <el-button v-hasPermi="['monitor:online:forceLogout']" icon="Delete" link type="primary" @click="handleForceLogout(scope.row)">强退</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
+    <div class="main-card">
+      <el-form ref="queryRef" :inline="true" :model="queryParams">
+        <el-form-item label="登录地址" prop="ipaddr">
+          <el-input v-model="queryParams.ipaddr" clearable placeholder="请输入登录地址" style="width: 200px" @keyup.enter="handleQuery" />
+        </el-form-item>
+        <el-form-item label="用户账号" prop="userName">
+          <el-input v-model="queryParams.userName" clearable placeholder="请输入用户账号" style="width: 200px" @keyup.enter="handleQuery" />
+        </el-form-item>
+        <el-form-item>
+          <el-button icon="Search" type="primary" @click="handleQuery">搜索</el-button>
+          <el-button icon="Refresh" @click="resetQuery">重置</el-button>
+        </el-form-item>
+      </el-form>
+      <el-table v-loading="loading" :data="onlineList" style="width: 100%">
+        <el-table-column align="center" label="序号" type="index" width="50">
+          <template #default="scope">
+            <span>{{ (pageNum - 1) * pageSize + scope.$index + 1 }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="会话编号" prop="tokenId" :show-overflow-tooltip="true" />
+        <el-table-column align="center" label="登录账号" prop="userName" :show-overflow-tooltip="true" />
+        <el-table-column align="center" label="所属部门" prop="deptName" :show-overflow-tooltip="true" />
+        <el-table-column align="center" label="主机" prop="ipaddr" :show-overflow-tooltip="true" />
+        <el-table-column align="center" label="登录地点" prop="loginLocation" :show-overflow-tooltip="true" />
+        <el-table-column align="center" label="操作系统" prop="os" :show-overflow-tooltip="true" />
+        <el-table-column align="center" label="浏览器" prop="browser" :show-overflow-tooltip="true" />
+        <el-table-column align="center" label="登录时间" prop="loginTime" width="180">
+          <template #default="scope">
+            <span>{{ parseTime(scope.row.loginTime) }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column align="center" class-name="small-padding fixed-width" label="操作">
+          <template #default="scope">
+            <el-button v-hasPermi="['monitor:online:forceLogout']" icon="Delete" link type="primary" @click="handleForceLogout(scope.row)">强退</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
 
-    <pagination v-show="total > 0" v-model:limit="pageSize" v-model:page="pageNum" :total="total" @pagination="getList" />
+      <pagination v-show="total > 0" v-model:limit="pageSize" v-model:page="pageNum" :total="total" @pagination="getList" />
+    </div>
   </div>
 </template>
 
