a274e8751492adb728d8d5552d48348e5aa9a53c
crazystudent13 <1076535690@qq.com>
2026-04-06 04:29:43 +0800

    refactor: 移除 Settings 和 UserProfile 中未使用的 proxy
    - Settings: 移除 getCurrentInstance 和 proxy
    - user/profile: 移除 getCurrentInstance 和 proxy，清理注释
    - 这些文件不再需要访问组件实例

--- admin-vue3/src/layout/components/Settings/index.vue
+++ admin-vue3/src/layout/components/Settings/index.vue
@@ -87,7 +87,6 @@ import useSettingsStore from '@/store/modules/settings'
 import usePermissionStore from '@/store/modules/permission'
 import { handleThemeStyle } from '@/utils/theme'
 
-const { proxy } = getCurrentInstance()
 const appStore = useAppStore()
 const settingsStore = useSettingsStore()
 const permissionStore = usePermissionStore()
