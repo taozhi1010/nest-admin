6452221297576144b375eb659ba159b4a836af94
crazystudent13 <1076535690@qq.com>
2026-04-06 06:23:06 +0800

    refactor(auth): 移除auth插件，改用useAuth composable

--- admin-vue3/src/plugins/index.js
+++ admin-vue3/src/plugins/index.js
@@ -1,11 +1,8 @@
 
-import auth from './auth'
 import cache from './cache'
 import download from './download'
 
 export default function installPlugins(app) {
-  // 认证对象
-  app.config.globalProperties.$auth = auth
   // 缓存对象
   app.config.globalProperties.$cache = cache
   // 下载文件
