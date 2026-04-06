3adf409995599256cb9f86b87b67cfc9f1c6a880
crazystudent13 <1076535690@qq.com>
2026-04-06 08:14:53 +0800

    refactor: 将submitNoEnter从mixin重构为TypeScript自定义指令
    - 创建 submitNoEnter.ts 自定义指令，使用 TypeScript 类型定义
    - 在 directive/index.js 中注册 v-no-enter 指令
    - 从 main.js 移除全局 mixin 注册
    - 删除旧的 utils/submitNoEnter.js 文件
    - 提供更灵活和符合 Vue 3 规范的实现方式

--- admin-vue3/src/directive/index.js
+++ admin-vue3/src/directive/index.js
@@ -1,9 +1,11 @@
 import hasRole from './permission/hasRole'
 import hasPermi from './permission/hasPermi'
 import copyText from './common/copyText'
+import submitNoEnter from './common/submitNoEnter'
 
 export default function directive(app) {
   app.directive('hasRole', hasRole)
   app.directive('hasPermi', hasPermi)
   app.directive('copyText', copyText)
+  app.directive('noEnter', submitNoEnter)
 }
