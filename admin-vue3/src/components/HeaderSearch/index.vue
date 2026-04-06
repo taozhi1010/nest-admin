e98b8f5512c69b344d0570005c03729648c97c68
crazystudent13 <1076535690@qq.com>
2026-04-06 08:06:33 +0800

    优化：路由搜索触发器改用el-input组件实现

--- admin-vue3/src/components/HeaderSearch/index.vue
+++ admin-vue3/src/components/HeaderSearch/index.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="header-search">
-    <svg-icon class-name="search-icon" icon-class="search" @click.stop="openSearch" />
+    <el-input
+      class="search-input"
+      readonly
+      placeholder="搜索路由..."
+      :prefix-icon="Search"
+      @click="openSearch"
+    >
+      <template #suffix>
+        <span class="kbd">Ctrl K</span>
+      </template>
+    </el-input>
     <SearchModal ref="searchModalRef" />
   </div>
 </template>
 
 <script setup>
+import { Search } from '@element-plus/icons-vue'
 import SearchModal from './components/SearchModal'
 
 const searchModalRef = ref(null)
@@ -17,12 +28,38 @@ function openSearch() {
 
 <style lang="scss" scoped>
 .header-search {
-  font-size: 0 !important;
-
-  .search-icon {
+  .search-input {
+    width: 200px;
     cursor: pointer;
-    font-size: 18px;
-    vertical-align: middle;
+
+    :deep(.el-input__wrapper) {
+      cursor: pointer;
+      background-color: #f5f7fa;
+      
+      &:hover {
+        background-color: #e4e7ed;
+      }
+    }
+
+    :deep(.el-input__inner) {
+      cursor: pointer;
+    }
+
+    .kbd {
+      display: inline-flex;
+      align-items: center;
+      justify-content: center;
+      min-width: 48px;
+      height: 24px;
+      padding: 0 6px;
+      font-size: 12px;
+      font-family: inherit;
+      color: #909399;
+      background: #fff;
+      border: 1px solid #dcdfe6;
+      border-radius: 6px;
+      box-shadow: 0 1px 0 rgba(0, 0, 0, 0.05);
+    }
   }
 }
 </style>
