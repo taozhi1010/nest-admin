3105a785e03a14586f7211ea5340cdc5d25d7b5b
crazystudent13 <1076535690@qq.com>
2026-04-18 19:19:13 +0800

    fix: 移除无用的game模块

--- server/src/module/game/game.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GameArticleModule } from './article/article.module';
-
-@Module({
-  imports: [GameArticleModule],
-  exports: [GameArticleModule],
-})
-export class GameModule {}
