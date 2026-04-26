1bda98a1aeb690c41f7c05e22d36562fd459a12d
crazystudent13 <1076535690@qq.com>
2026-04-27 01:29:31 +0800

    refactor: 将非API的JS文件转换为TypeScript
    - requireIcons.js  requireIcons.ts (图标加载工具)
    - svgicon.js  svgicon.ts (Element Plus图标插件)
    - layout/components/index.js  index.ts (布局组件导出)
    - 添加类型定义和JSDoc注释
    - 构建测试通过，无错误

--- admin-vue3/src/components/SvgIcon/svgicon.ts
+++ admin-vue3/src/components/SvgIcon/svgicon.ts
@@ -1,7 +1,13 @@
+/**
+ * Element Plus 图标全局注册插件
+ * 自动注册所有 Element Plus 图标组件
+ */
+
+import type { App } from 'vue'
 import * as components from '@element-plus/icons-vue'
 
 export default {
-  install: (app) => {
+  install: (app: App) => {
     for (const key in components) {
       const componentConfig = components[key]
       app.component(componentConfig.name, componentConfig)
---
+++ admin-vue3/src/layout/components/index.ts
@@ -0,0 +1,8 @@
+/**
+ * 布局组件统一导出
+ */
+
+export { default as AppMain } from './AppMain.vue'
+export { default as Navbar } from './Navbar.vue'
+export { default as Settings } from './Settings/index.vue'
+export { default as TagsView } from './TagsView/index.vue'
