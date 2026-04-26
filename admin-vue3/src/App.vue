0ee9e5a598314f10a8008f100db09fbda7637f0e
crazystudent13 <1076535690@qq.com>
2026-04-27 02:07:21 +0800

    fix: 修复App.vue编码问题

--- admin-vue3/src/App.vue
+++ admin-vue3/src/App.vue
@@ -8,7 +8,8 @@ import { handleThemeStyle } from '@/utils/theme'
 
 onMounted(() => {
   nextTick(() => {
-    // 初始化主题样�?    handleThemeStyle(useSettingsStore().theme)
+    // 初始化主题样式
+    handleThemeStyle(useSettingsStore().theme)
   })
 })
 </script>
